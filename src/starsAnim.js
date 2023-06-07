fe21af0191384dffec7e102af84654c68ea98a78
aurore-ry <aremy.dev@gmail.com>
2023-06-07 20:03:10 +0200

    add stars animation & continue html architecture

--- src/starsAnim.js
+++ src/starsAnim.js
@@ -1,31 +1,117 @@
-// function createStars(_i) {
-//   for (let i; i; i--) {
-//     drawStars();
-//   }
-// }
+let parent = document.querySelector("#heading_layout");
 
-// function drawStars() {
-//   let tmpStar = document.createElement("figure");
-//   tmpStar.className = "star";
-//   tmpStar.style.top = 100 * Math.random() + "%";
-//   tmpStar.style.left = 100 * Math.random() + "%";
-//   document.getElementById("stars").appendChild(tmpStar);
-// }
+const { width: parentWidth, height: parentHeight } =
+  parent.getBoundingClientRect();
 
-// function selectStars() {
-//   const stars = document.querySelectorAll(".star");
-//   console.log(stars);
-// }
+const canvas = document.getElementById("canvas");
+canvas.width = parentWidth;
+canvas.height = parentHeight;
 
-// function animateStars() {
-//   stars.forEach(function (el, i) {
-//     TweenMax.to(el, Math.random() * 0.5 + 0.5, {
-//       opacity: Math.random(),
-//       onComplete: animateStars,
-//     });
-//   });
-// }
+const ctx = canvas.getContext("2d");
 
-// createStars(200);
-// selectStars();
-// animateStars();
+function resizeCanvas() {
+  let parent = document.querySelector("#heading_layout");
+  const parentSize = parent.getBoundingClientRect();
+
+  const canvas = document.getElementById("canvas");
+  canvas.width = parentSize.width;
+  canvas.height = parentSize.height;
+}
+
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+let starsColors = [
+  "#ebfcfe",
+  "#d2fafd",
+  "#fefeca",
+  "#FFFFFF",
+  "#ffff99",
+  "#ccffff",
+];
+function getRandomColor(array) {
+  if (starsColors.length == 0) {
+    return undefined;
+  }
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+class Star {
+  constructor() {
+    this.x = 0;
+    this.y = 0;
+    this.vy = 1;
+    this.radius = 0.9;
+    this.alpha = 0.5;
+    this.color = getRandomColor(starsColors);
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.closePath();
+
+    ctx.save();
+
+    const globalAlpha = ctx.globalAlpha;
+    ctx.globalAlpha = this.alpha;
+
+    ctx.fillStyle = this.color;
+    ctx.fill;
+    ctx.fill();
+
+    ctx.globalAlpha = globalAlpha;
+    ctx.restore();
+  }
+}
+
+function makeRandomStar() {
+  const canvasSize = canvas.getBoundingClientRect();
+  randomStar = new Star();
+  randomStar.x = randomNumber(0, canvasSize.width - randomStar.radius);
+  randomStar.y = randomNumber(0, canvasSize.height - randomStar.radius);
+  randomStar.vy = randomNumber(1.2, 1.4);
+  randomStar.alpha = randomNumber(0, 1);
+  return randomStar;
+}
+
+let stars = [];
+
+function initScene() {
+  for (i = 0; i < 1000; i++) {
+    stars.push(makeRandomStar());
+  }
+}
+
+function mainloop() {
+  const canvasSize = canvas.getBoundingClientRect();
+  ctx.clearRect(0, 0, canvasSize.width, canvasSize.height);
+
+  for (let i = 0; i < stars.length - 1; i++) {
+    if (stars[i].y > 0) {
+      stars[i].y -= 1 * stars[i].vy;
+      if (Math.random() > 0.8) {
+        stars[i].radius = randomNumber(0.9, 1.8);
+      }
+    } else {
+      stars[i] = makeRandomStar();
+    }
+
+    stars[i].draw();
+  }
+
+  requestAnimationFrame(mainloop);
+}
+
+resizeCanvas();
+window.addEventListener(
+  "resize",
+  function () {
+    resizeCanvas();
+  },
+  false
+);
+
+initScene();
+mainloop();
